8140c3c75d0f35ac7abd82c1cf32ed0ace955eea
Julian Steffen <james14@mail.de>
2017-11-28 22:15:01 +0100

    socket.io

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
@@ -13,6 +17,6 @@ app.get('/', (req, res) => {
   res.send(index.html);
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on Port ${port}`);
 });
