8c2beb5d3dd3e9f84ebe07c5aca38a59eb5bfe8c
Julian Steffen <james14@mail.de>
2017-12-07 20:17:06 +0100

    Final

--- playground/time.js
+++ playground/time.js
@@ -1,8 +1,21 @@
-const moment = require('moment');
+var moment = require('moment');
 
-const date = moment();
-console.log(date.format('MMM Do, YYYY'))
+// Jan 1st 1970 00:00:10 am
 
-const time = moment();
-console.log(time.format('h:mm a'));
-console.log(time.format('H:mm a'));
+// var date = new Date();
+// var months = ['Jan', 'Feb']
+// console.log(date.getMonth());
+
+// var date = moment();
+// date.add(100, 'year').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY'));
+
+// 10:35 am
+// 6:01 am
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp)
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'))
